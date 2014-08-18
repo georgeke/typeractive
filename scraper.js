d285141a88cc2bef79ce420f5a28f372ee7d5b13
George Ke <george-ke@hotmail.com>
2014-08-17 22:50:37 -0400

    Added HTML parsing.

--- scraper.js
+++ scraper.js
@@ -15,16 +15,40 @@ Scraper = {
 		$.getJSON(this.base, payload, function(data) {
 			debugger;
 			pages = data['query']['pages'];
-			pars = {};
+			text = {};
 
 			for(var key in pages) {
 				if(pages.hasOwnProperty(key)) {
-					pars[pages[key]['title']] = pages[key]['extract'];
+					text[pages[key]['title']] = pages[key]['extract'];
 				}
 			}
 
-			for (var title in pars) {
-				$('#output').html(pars[title]);
+			for (var title in text) {
+				var textObj = $.parseHTML(text[title]);
+				var paras = [];
+				for (var i=0 ; i<textObj.length ; ++i) {
+					// Get text from all paragraphs that have a period in them.
+					var innerText = textObj[i]['innerText']
+					if (textObj[i]['tagName'] === "P" && innerText.indexOf(".") > -1) {
+						paras.push(innerText)
+					}
+				}
+
+				/*
+				var temp = $('<div></div');
+				temp.html(text[title]);
+				var paras = $('p', temp)
+
+				var all = ''
+				alert(paras.length)
+				for (var p in paras) {
+					if (paras.hasOwnProperty(p))
+						$('#output').append(paras[p]);
+				}*/
+
+				//$('#output').html(all);
+
+
 			}
 		});
 	}
