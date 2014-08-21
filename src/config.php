2f7abb53f69b58b1c9814c5c0f0df59c5311b182
George Ke <george-ke@hotmail.com>
2014-08-20 23:12:02 -0400

    Fixed scraping issue in Firefox. \nOrganized dirs.

--- src/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	define('URL', 'http://en.wikipedia.org/w/api.php?format=json');
-	define('Username', 'dogs');
-    define('Password', 'and_cats');
-
-	$config = array(
-		'dbname' => 'finalTest',
-    );
-?>
