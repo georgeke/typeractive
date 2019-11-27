518150cc2558ed9bb97364b7b2216cf887c6fa74
George Ke <georgekeee@gmail.com>
2019-11-26 23:14:18 -0500

    make scraper just output in html

--- src/js/scraper.js
+++ src/js/scraper.js
@@ -1,75 +1,80 @@
 Scraper = {
     base: 'http://en.wikipedia.org/w/api.php?format=json&callback=?&redirects',
-    paras: null,
+    catToParagraphs: {},
+    curArticleName: '',
+    numArticlesParsed: 0,
+    numArticles: 0,
 
     /***
-     * Runs parsePage on each article name in the input field
+     * Runs readWiki on each article name in the input field
      */
     parseInput: function() {
-        var input = $('#urlForm').val().trim();
-        var articleNames = input.split(/\s+/);
+        const input = $('#urlForm').val().trim();
+        const articleNames = input.split(/\s+/);
+        this.numArticles = articleNames.length
 
-        for (var i = 0 ; i < articleNames.length ; i++) {
-            debugger;
-            this.parsePage(articleNames[i]);
+        for (let i = 0 ; i < this.numArticles ; i++) {
+            this.readWiki(articleNames[i]);
         }
     },
 
     /***
      * Extracts information from articleName and sends AJAX request to mongoDB
      */
-    parsePage: function(articleName) {
-        var payload = {
+    readWiki: function(articleName) {
+        const payload = {
             action: 'query',
             prop: 'extracts',
             titles: articleName
         };
+        this.curArticleName = articleName
 
-        // Get array of all paragraphs, placed in paras
-        $.getJSON(this.base, payload, function(data) {
-            var pages = data['query']['pages'];
-            var text = {};
+        // Get array of all paragraphs
+        // TODO: A cookie associated with a cross-site resource at https://en.wikipedia.org/ was set without
+        // the `SameSite` attribute. A future release of Chrome will only deliver cookies with cross-site requests
+        // if they are set with `SameSite=None` and `Secure`. You can review cookies in developer tools under
+        // Application>Storage>Cookies and see more details at https://www.chromestatus.com/feature/5088147346030592
+        // and https://www.chromestatus.com/feature/5633521622188032.
+        $.getJSON(this.base, payload, this.parseWikiData.bind(this));
+    },
 
-            for(var key in pages) {
-                if(pages.hasOwnProperty(key)) {
-                    text[pages[key]['title']] = pages[key]['extract'];
+    parseWikiData: function(data) {
+        const pages = data['query']['pages'];
+        let text = {};
+
+        for (let key in pages) {
+            if (pages.hasOwnProperty(key)) {
+                text[pages[key]['title']] = pages[key]['extract'];
+            }
+        }
+
+        let paras = [];
+        for (let title in text) {
+            const textObj = $.parseHTML(text[title]);
+
+            for (let i = 0 ; i < textObj.length ; ++i) {
+                // Get text from all paragraphs that have a period in them.
+                const innerText = textObj[i]['textContent'];
+                // Thanks IE...
+                if (innerText === undefined) {
+                    innerText = textObj[i]['innerText'];
+                }
+                if (textObj[i]['tagName'] === 'P' && innerText.indexOf('.') > -1) {
+                    paras.push(innerText);
                 }
             }
+        }
 
-            for (var title in text) {
-                var textObj = $.parseHTML(text[title]);
-                var paras = [];
-                var all = '';
+        this.catToParagraphs[this.curArticleName] = paras;
+        this.numArticlesParsed++;
+        $('#output').html(`${this.numArticlesParsed}/${this.numArticles} articles parsed`);
 
-                for (var i=0 ; i<textObj.length ; ++i) {
-                    // Get text from all paragraphs that have a period in them.
-                    var innerText = textObj[i]['textContent'];
-                    // Thanks IE...
-                    if (innerText===undefined) {
-                        innerText = textObj[i]['innerText'];
-                    }
-                    if (textObj[i]['tagName'] === "P" && innerText.indexOf(".") > -1) {
-                        all += innerText;
-                        paras.push(innerText);
-                    }
-                }
-                //$('#output').html(all);
-                Scraper.paras = paras;
-            }
+        if (this.numArticlesParsed === this.numArticles) {
+            this.numArticlesParsed = 0
+        }
+    },
 
-            // Send post request to database
-            var allData = {'collection':articleName};
-            for (var i = 0 ; i < Scraper.paras.length ; ++i) {
-                allData['p'+i] = Scraper.paras[i];
-            }
-            $.ajax({
-                url: "res/dbFunc.php",
-                type: "POST",
-                data: allData,
-                success: function(data, text) {
-                    $("#msg").html(data);
-                }
-            });
-        });
+    displayJSON: function() {
+        $('#output').html(JSON.stringify(this.catToParagraphs));
     }
 };
